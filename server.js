4ff40a8d9d346128da5053da35b8cec5255fbdf1
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-04 23:33:41 +0000

    Adding basic routing to the simple server. Handles the 'About' and 'Contact' pages, everything else gets the basic message.

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ var http = require('http');
 http.createServer(function (req, res){
 	// Editing server to render HTML instead
 	res.writeHead(200, {'Content-Type': 'text/html'}); // http header looks for HTML
-	res.write('<h1>Hello World!</h1>'); // simple response
-	res.end(); // end the response
+	
+	var url = req.url;
+	if (url === '/about'){
+		res.write('<h1>Welcome To The About Page!</h1>'); // simple response
+		res.end(); // end the response
+	} else if(url === '/contact'){
+		res.write('<h1>Welcome To The Contact Page!</h1>');
+		res.end();
+	} else {
+		res.write('<h1>Hello World!</h1>');
+		res.end();
+	}
 }).listen(process.env.PORT, function(){
 	console.log(`Server listening on port ${process.env.PORT}`); // server listening via env variable
 });
