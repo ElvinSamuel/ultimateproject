1b0fd6478ac99b8e32e03d0360026ec535442c31
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-04 23:25:55 +0000

    Added 'res.writeHead() to render basic HTML in the NodeJS server

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var http = require('http');
 
 // simple server
 http.createServer(function (req, res){
-	res.write('Hello World!'); // simple response
+	// Editing server to render HTML instead
+	res.writeHead(200, {'Content-Type': 'text/html'}); // http header looks for HTML
+	res.write('<h1>Hello World!</h1>'); // simple response
 	res.end(); // end the response
 }).listen(process.env.PORT, function(){
 	console.log(`Server listening on port ${process.env.PORT}`); // server listening via env variable
