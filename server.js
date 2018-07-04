c13ef637d48257fea4dab862c59092329edb19f3
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-04 23:55:41 +0000

    Created a simple File Server that renders a separate file when a request is sent. All still without Express for now.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // import the http module from Node
 var http = require('http');
+var fs   = require('fs');
 
 // simple server
 http.createServer(function (req, res){
-	// Editing server to render HTML instead
-	res.writeHead(200, {'Content-Type': 'text/html'}); // http header looks for HTML
-	res.write('<h1>Hello World!</h1>'); // simple response
-	res.end(); // end the response
+	fs.readFile('index.html', function(err, data){
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		res.end();
+	});
 }).listen(process.env.PORT, function(){
 	console.log(`Server listening on port ${process.env.PORT}`); // server listening via env variable
 });
