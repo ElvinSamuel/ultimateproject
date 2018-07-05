d9d7cb4d71e893c41d286ff116d61a41140b24e1
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-05 19:01:23 +0000

    Updated the gulpfile with 'gulp-sass' and adding basic routing for the pre-compiler.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+sass = require('gulp-sass');
 
-gulp.task('hello', function(){
-	console.log('Hello, Elvin');
+gulp.task('sass', function(){
+	return gulp.src('./views/sass/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('./public/css')
 });
