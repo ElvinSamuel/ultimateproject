e7bf0b613b3eced08ed6e3afb9c65be1c9076322
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-05 03:16:49 +0000

    Added ./routes and express.Routes() to the project.

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express'),
     app     = express();
 
-app.get('/', function(req, res){
-	res.send('Hello World!');
-});
+var index = require('./routes/index.js');
+// ==========================
+// My Routes
+// ==========================
+
+   app.use('/', index);
+// ==========================
 
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log(`Ultimate Web Developer Running On Port ${process.env.PORT}`);
