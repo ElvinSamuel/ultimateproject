a3fcabb48e7ee2f82a3a358e084f41a37bf618a9
Elvin Samuel <elvinsamuel1@gmail.com>
2018-07-05 19:25:29 +0000

    Updated gulpfile with 'gulp watch' and first successful .css file compiled in 'public/css'

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,11 @@ var gulp = require('gulp');
 sass = require('gulp-sass');
 
 gulp.task('sass', function(){
-	return gulp.src('./views/sass/*.scss')
+	return gulp.src('views/sass/main.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('./public/css')
+		.pipe(gulp.dest('public/css'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('./views/sass/*.scss', ['sass']);
 });
